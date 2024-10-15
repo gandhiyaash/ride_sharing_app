cc2358b3562df18f9ab44cc9bc22c41d290da38c
Yash Gandhi <yashgandhi.2408@gmail.com>
2024-10-15 14:30:45 -0400

    creating a rider class

---
+++ rider.py
@@ -0,0 +1,14 @@
+class Rider:
+    def __init__(self, name):
+        self.name = name
+        self.ride_count = 0
+        self.is_preffered = False
+        self.active_ride = []
+
+    def update_ride_count(self):
+        self.ride_count += 1
+        if self.ride_count > 10:
+            self.is_preffered= True
+    
+    def __repr__(self):
+        return f"Rider(name={self.name}, rides={self.ride_count}, preffered={self.is_preffered})"
